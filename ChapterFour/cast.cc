0c1c07473d5df10d8dca7e0688b435161bcdb636
Micha <michael.burkhardt@student.fh-kiel.de>
2022-01-28 17:58:20 +0100

    finished chapter five

---
+++ ChapterFour/cast.cc
@@ -0,0 +1,16 @@
+/*
+Typecast possibilities
+
+static_cast<TYPE>(expr)
+
+dynamic_cast<TYPE>(expr)
+later
+
+const_cast<TYPE>(expr)
+deactivates a const deklaration (better don't do this)
+
+reinterpret_cast<TYPE>(expr)
+use only with raw bits and binary bytes
+
+forget about old c casts
+*/
